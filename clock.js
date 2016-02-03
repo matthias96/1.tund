f204add7ab9a37be61f652d51438c3fefa6076e2
Matthias Johann Kurs <mattkur@greeny.cs.tlu.ee>
2016-02-03 11:07:13 +0200

    Its a clock

---
+++ clock.js
@@ -0,0 +1,25 @@
+
+window.onload= function(){
+  var clock= document.getElementById('clock');
+  writeDate();
+
+};
+
+function writeDate(){
+  var today = new Date();
+
+  var hours = addZeroBefore(today.getHours());
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+  seconds = addZeroBefore(seconds);
+
+  clock.innerHTML = hours + ':' + addZeroBefore(minutes) + ':' + seconds;
+}
+
+function addZeroBefore(number){
+  if(number < 10){
+    number = "0" + number;
+  }
+
+  return number;
+}
